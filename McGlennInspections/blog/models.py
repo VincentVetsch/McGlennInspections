0f69f9fc78a51bc018eb1b8dc9a8f02fea4f5ff0
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-07 15:15:01 -0600

    Added slug field and unicode object

--- McGlennInspections/blog/models.py
+++ McGlennInspections/blog/models.py
@@ -7,9 +7,12 @@ class Posts(models.Model):
 
     author = models.CharField(max_length=30)
     title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
     bodytext = models.TextField()
     timestamp = models.DateTimeField()
     '''TODO - Add the following fields
         image field
-        slug field
     '''
+
+    def __unicode__(self):
+        return self.name
