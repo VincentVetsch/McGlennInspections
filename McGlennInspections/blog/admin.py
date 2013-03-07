a4b09377f58c404df137c77cddca4f125e3ec37b
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-07 15:40:19 -0600

    Created blogthumbs directory and moved Home.png.  Commented PostAdmin Class

--- McGlennInspections/blog/admin.py
+++ McGlennInspections/blog/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from blog.models import Post
-# Register your models here. from beer.models import Beer, Brewery
+# Register your models here.
 
 
 class PostAdmin(admin.ModelAdmin):
-        prepopulated_fields = {'slug': ('title',)}
-        list_display = ('title', 'author', 'bodytext', 'timestamp')
-        search_fields = ['title']
+    ''' Admin module for post model '''
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'bodytext', 'timestamp')
+    search_fields = ['title']
 
 admin.site.register(Post, PostAdmin)
