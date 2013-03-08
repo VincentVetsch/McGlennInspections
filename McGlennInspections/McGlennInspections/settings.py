2e43cc058df461be79be06df76e45e62e0cb2ea5
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-07 18:39:14 -0600

    Added database and mysql user, this is currently only useful on local machine

--- McGlennInspections/McGlennInspections/settings.py
+++ McGlennInspections/McGlennInspections/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'McGlennInspections.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mcglenn',
+        'USER': 'mcglenn',
+        'PASSWORD': 'BG5CpLGEntWnAhxe',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
