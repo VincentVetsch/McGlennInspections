a3e45d0a5d27d312be99e993c27b653b35909021
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-08 10:45:29 -0600

    Registered admin functionality

--- McGlennInspections/appointment/admin.py
+++ McGlennInspections/appointment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from appointment.models import Appointment
 # Register your models here.
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    ''' Admin module for appointment model '''
+    prepopulated_fields = {'slug': ('title',)}
+    #TODO - Add proper fields
+    list_display = ('title', 'author', 'bodytext', 'timestamp')
+    search_fields = ['title']
+
+admin.site.register(Appointment, AppointmentAdmin)
--- McGlennInspections/rvalues/admin.py
+++ McGlennInspections/rvalues/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rvalues.models import RValue
+
 
 # Register your models here.
+class RValueAdmin(admin.ModelAdmin):
+    ''' Admin module for Rvalues model '''
+    prepopulated_fields = {'slug': ('title',)}
+    #TODO - Add proper fields
+    list_display = ('title', 'author', 'bodytext', 'timestamp')
+    search_fields = ['title']
+
+admin.site.register(RValue, RValueAdmin)
