3c59281e1814bb6a46f38ccef562977e0c6a27c5
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-11 17:04:27 -0500

    Added table fields to slideshow

--- McGlennInspections/slideshow/models.py
+++ McGlennInspections/slideshow/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Slide(models.Model):
+    '''
+        Fields for Slide Table
+    '''
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    bodytext = models.TextField()
+    timestamp = models.DateTimeField()
+    image = models.ImageField(upload_to="images/slideshow/", help_text="600x400px image")
+
+    def __unicode__(self):
+        return self.title
