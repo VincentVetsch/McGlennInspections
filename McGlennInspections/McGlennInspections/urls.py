00c327065ba870d748a97425c77333b756d5572c
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-13 17:17:02 -0500

    Fixed url error for blog view

--- McGlennInspections/McGlennInspections/urls.py
+++ McGlennInspections/McGlennInspections/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'McGlennInspections.blog.views.home', name='blog'),
+    url(r'^blog/', 'McGlennInspections.blog.views.home', name='blog'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     #url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
