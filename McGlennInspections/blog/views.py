340383ae903cbc0b6ce6e67b5efeed61933c7b68
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-13 19:23:17 -0500

    Added Comments to functions

--- McGlennInspections/blog/views.py
+++ McGlennInspections/blog/views.py
@@ -4,11 +4,16 @@ from blog.models import Posts
 
 def blog_page(request):
     '''
-        This is the view for blog
+        This is the main view for all blog entries
+        Returns: Page with content values
     '''
     # TODO - Start adding the content to the page
     return render_to_response("blog.html", {'blog': Posts})
 
 
 def blog_page_details(request):
+    '''
+        This is the detail view for each blog entries
+        Returns: Page with content values
+    '''
     pass
