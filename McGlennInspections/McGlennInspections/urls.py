59853495c11540fb8615f60f0d21d9e0e1d647bc
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-13 19:02:42 -0500

    Added all app urls to site urls

--- McGlennInspections/McGlennInspections/urls.py
+++ McGlennInspections/McGlennInspections/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
+    url(r'^appointment/', include('appointment.urls')),
+    url(r'^glossary/', include('glossary.urls')),
+    url(r'^slideshow/', include('slideshow.urls')),
+    url(r'^rvalues/', include('rvalues.urls')),
 )
