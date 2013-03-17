6225a68cf7c488e83da384477fd4784a6a5f5c40
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-16 20:34:44 -0500

    Modified entries to order by timestamp

--- McGlennInspections/blog/views.py
+++ McGlennInspections/blog/views.py
@@ -1,26 +1,46 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from blog.models import Posts
+#import vimpdb
+#vimpdb.set_trace()
 
 sitename = "McGlenn Home Inspections"
 
 
 def blog_page(request):
     '''
-        This is the main view for all blog entries
+        blog_page: This is the main view for all blog entries
+
+        Arguments:
+             request
         Returns: Page with content values
     '''
     # TODO - Start adding the content to the page
-    # TODO - Order by date
-    entries = Posts.objects.all()
+    entries = Posts.objects.order_by('-timestamp')
     content = {'blog': entries, 'site': sitename}
     return render_to_response("blog.html", content, context_instance=RequestContext(request))
 
 
 def blog_page_details(request):
     '''
-        This is the detail view for each blog entries
+        blog_page_details: This is the detail view for each blog entries
+
+        Arguments:
+             request
         Returns: Page with content values
+    '''
+    # TODO - Start adding the content to the page
+    pass
+
+
+def blog_comments(request):
+    '''
+        blog_comments:  This is a page for creating comments on blog entries.
+
+        Arguments:
+            request
+        Return:
+            render_to_response of page
     '''
     # TODO - Start adding the content to the page
     pass
