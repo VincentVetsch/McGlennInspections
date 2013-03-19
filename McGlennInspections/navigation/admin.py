07eb26efffedaea98863a988c9ed79b8ea29cea3
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-18 21:05:08 -0500

    Registered Navigation admin

--- McGlennInspections/navigation/admin.py
+++ McGlennInspections/navigation/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from navigation.models import Navigation
+
+
+class NavigationAdmin(admin.ModelAdmin):
+    '''
+        Admin module for post model
+    '''
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title']
+
+admin.site.register(Navigation, NavigationAdmin)
 
 # Register your models here.
