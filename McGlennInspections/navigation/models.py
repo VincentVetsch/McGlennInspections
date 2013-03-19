42b4f3ce9790e20dee7b48e7521cdf070f7630c1
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-18 20:51:27 -0500

    Added comments and icon_field and choices for link type.

--- McGlennInspections/navigation/models.py
+++ McGlennInspections/navigation/models.py
@@ -2,17 +2,24 @@ from django.db import models
 TYPE_CHOICE = (
     ('P', 'Parent'),
     ('C', 'Child'),
+    ('I', 'Individual')
 )
 
 
 # Create your models here.
 class Navigation(models.Model):
     '''
-        Navigation:  This model is for creating site navigation links
+        Navigation:  This model is for creating site navigation
+        links.  This application should be used as an extension
+        for the base.html file for the site or a extension of
+        other templates.  This should help easily manage links
+        for the site.  There shouldn't be any views.py, just the
+        admin.py.
     '''
 
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+    # TODO - Research models.UrlField for links
     link = models.CharField(max_length=200, blank=True)
     # Type is either parent or child
     type_of_link = models.CharField(max_length=1, choices=TYPE_CHOICE)
@@ -21,6 +28,11 @@ class Navigation(models.Model):
     # Level of the navigation
     level = models.IntegerField()
     div_class = models.CharField(max_length=20)
+    icon_field = models.ImageField(
+        upload_to="images/navigation/",
+        help_text="32x32px image",
+        blank=True
+    )
 
     def __unicode__(self):
         return self.title
