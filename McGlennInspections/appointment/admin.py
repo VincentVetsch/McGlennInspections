a17a4591a156d94174dcb8d4d68d1d77c3607d8f
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-20 14:01:49 -0500

    Added new relations and admin modules

--- McGlennInspections/appointment/admin.py
+++ McGlennInspections/appointment/admin.py
@@ -1,34 +1,79 @@
 from django.contrib import admin
-from appointment.models import Appointment
+from appointment.models import Appointment, Feedback, CustomerInformation, InspectionAddress, HouseInformation
 
 
 # Register your models here.
+# DONE - Add preview fields for list view
 class AppointmentAdmin(admin.ModelAdmin):
-    ''' Admin module for appointment model '''
-    prepopulated_fields = {'slug': ('last_name', 'first_name',)}
+    ''' Admin module for appointment model
+    '''
+    prepopulated_fields = {'slug': ('full_name',)}
     list_display = (
-        'first_name',
-        'last_name',
+        'full_name',
+        'date_requested',
+        'time_requested',
+        'timestamp',
+    )
+    search_fields = ['full_name']
+
+
+# DONE - Add preview fields for list view
+class FeedbackAdmin(admin.ModelAdmin):
+    ''' Admin for Feedback model
+    '''
+    prepopulated_fields = {'slug': ('full_name',)}
+    list_display = (
+        'full_name',
+        'timestamp',
+        'email',
+        'approve',
+    )
+    search_fields = ['full_name']
+
+
+# DONE - Add preview fields for list view
+class CustomerInformationAdmin(admin.ModelAdmin):
+    ''' Admin for Feedback model
+    '''
+    prepopulated_fields = {'slug': ('full_name',)}
+    list_display = (
+        'full_name',
         'phone',
         'mobile',
         'email',
-        'isp_address',
-        'isp_city',
-        'isp_state',
-        'isp_zip',
-        'sq_footage',
-        'basement',
-        'crawlspace',
-        'dining_room',
-        'living_room',
-        'family_room',
-        'number_of_bedrooms',
-        'number_of_bathrooms',
-        'date_req',
-        'time_req',
-        'notes',
-        'timestamp',
     )
-    search_fields = ['last_name', 'first_name']
+    search_fields = ['full_name']
+
+
+# DONE - Add preview fields for list view
+class InspectionAddressAdmin(admin.ModelAdmin):
+    ''' Admin for Feedback model
+    '''
+    prepopulated_fields = {'slug': ('full_name',)}
+    list_display = (
+        'full_name',
+        'inspection_address',
+        'inspection_city',
+        'inspection_zip',
+    )
+    search_fields = ['full_name', 'inspection_city', 'inspection_zip']
+
+
+# DONE - Add preview fields for list view
+class HouseInformationAdmin(admin.ModelAdmin):
+    ''' Admin for Feedback model
+    '''
+    prepopulated_fields = {'slug': ('full_name',)}
+    list_display = (
+        'full_name',
+        'address',
+        'square_footage',
+    )
+    search_fields = ['full_name']
+
 
 admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(CustomerInformation, CustomerInformationAdmin)
+admin.site.register(InspectionAddress, InspectionAddressAdmin)
+admin.site.register(HouseInformation, HouseInformationAdmin)
--- McGlennInspections/appointment/models.py
+++ McGlennInspections/appointment/models.py
@@ -1,37 +1,84 @@
 from django.db import models
 
 
-# Create your models here.
-class Appointment(models.Model):
-    '''Fields for Posts Table'''
-    #Customer Information
+class CustomerInformation(models.Model):
+    ''' Customer Information
+    '''
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    phone = models.CharField(max_length=20)
-    mobile = models.CharField(max_length=20)
-    email = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    #Address
-    isp_address = models.CharField(max_length=100)
-    isp_city = models.CharField(max_length=50)
-    isp_state = models.CharField(max_length=50)
-    isp_zip = models.IntegerField()
-    #House information
-    sq_footage = models.IntegerField()
+    email = models.EmailField(max_length=200, unique=True)
+    phone = models.CharField(max_length=20, unique=True)
+    mobile = models.CharField(max_length=20, unique=True)
+
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name
+
+
+class InspectionAddress(models.Model):
+    ''' The address of the inspection
+    '''
+    full_name = models.ForeignKey(CustomerInformation)
+    inspection_address = models.CharField(max_length=100)
+    inspection_city = models.CharField(max_length=50)
+    inspection_state = models.CharField(max_length=50)
+    inspection_zip = models.IntegerField()
+
+    def __unicode__(self):
+        return self.inspection_address
+
+
+class HouseInformation(models.Model):
+    ''' House information
+    '''
+    full_name = models.ForeignKey(CustomerInformation)
+    address = models.ForeignKey(InspectionAddress)
+    square_footage = models.IntegerField()
     basement = models.BooleanField()
-    garage = models.BooleanField()
     crawlspace = models.BooleanField()
+    garage = models.BooleanField()
+    out_buildings = models.BooleanField()
     dining_room = models.BooleanField()
     living_room = models.BooleanField()
     family_room = models.BooleanField()
     number_of_bedrooms = models.IntegerField()
     number_of_bathrooms = models.IntegerField()
+
+    def __unicode__(self):
+        return self.inspection_address
+
+
+class Appointment(models.Model):
+    ''' Fields for Posts Table
+    '''
+    #Customer information
+    full_name = models.ForeignKey(CustomerInformation)
+    slug = models.SlugField(unique=True)
+    #Inspection Address
+    address = models.ForeignKey(InspectionAddress)
     #Inspection details
-    date_req = models.DateField(auto_now=False, auto_now_add=False)
-    time_req = models.TimeField(auto_now=False, auto_now_add=False)
+    home = models.ForeignKey(HouseInformation)
+    date_requested = models.DateField(auto_now=False, auto_now_add=False)
+    time_requested = models.TimeField(auto_now=False, auto_now_add=False)
     #Notes
     notes = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Feedback(models.Model):
+    '''Fields for Posts Table
+    '''
+    full_name = models.ForeignKey(CustomerInformation)
+    email = models.ForeignKey(CustomerInformation, to_field='email', related_name='+')
+    slug = models.SlugField(unique=True)
+    #Address
+    inspection_address = models.ForeignKey(InspectionAddress)
+    #Notes
+    comments = models.TextField()
     timestamp = models.DateTimeField()
+    approve = models.BooleanField()
 
     def __unicode__(self):
         return self.title
