2a60d6d23d949ecd6ac0501b9e3a11567da62e4e
Vince Vetsch <vincent.vetsch@gmail.com>
2013-03-20 22:36:22 -0500

    Removed cust_feedback Application, merged it with appointments Application

--- McGlennInspections/cust_feedback/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from cust_feedback import views
-
-
-urlpatterns = patterns('',
-    url(r'^$', views.cust_feedback_page, name='cust_feedback'),
-    url(r'^details', views.cust_feedback_page_details, name='cust_feedback_details'),
-)
